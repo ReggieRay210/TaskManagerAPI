004de72ab320eeff0e52352febcbe143a1c8f17e
Reginald Griffin II <74481504+ReggieRay210@users.noreply.github.com>
2026-04-17 12:51:26 -0500

    Add comments for optional database creation
    Added comments to clarify database creation process.

--- init_db.py
+++ init_db.py
@@ -1,6 +1,9 @@
 from app.database import engine, Base
 from app import models
 
+# This is optional, the database will be created automatically 
+# under the main.py file.
+
 print("Creating database tables...")
 Base.metadata.create_all(bind=engine)
 print("Done! Database file: tasks.db")
